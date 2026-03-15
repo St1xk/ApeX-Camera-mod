fd7f4f928c3a7ad17cca96d8f8c3b9df76440a8d
St1ck <apex-camera-mod@github.com>
2026-03-15 17:27:15 +0100

    Remove network endpoints - now fully offline compatible

--- StickCameraMod/Version Checking/VersionChecker.cs
+++ StickCameraMod/Version Checking/VersionChecker.cs
@@ -1,6 +1,3 @@
-using System.IO;
-using System.Net.Http;
-using StickCameraMod.Core;
 using UnityEngine;
 
 namespace StickCameraMod.Version_Checking;
@@ -9,23 +6,8 @@ public class VersionChecker : MonoBehaviour
 {
     private void Start()
     {
-        using HttpClient client = new();
-        HttpResponseMessage response = client
-                                      .GetAsync(
-                                               "https://raw.githubusercontent.com/HanSolo1000Falcon/StickCameraMod/master/version.txt")
-                                      .Result;
-
-        response.EnsureSuccessStatusCode();
-        using Stream       stream   = response.Content.ReadAsStreamAsync().Result;
-        using StreamReader reader   = new(stream);
-        string             contents = reader.ReadToEnd().Trim();
-
-        string[] parts = contents.Split(";");
-
-        Version mostUpToDateVersion = new(parts[0]);
-        Version currentVersion      = new(Constants.PluginVersion);
-
-        if (currentVersion < mostUpToDateVersion)
-            CoreHandler.Instance.OnDeprecatedVersionDetected(currentVersion, mostUpToDateVersion, parts[1]);
+        // Version checking endpoint removed for offline compatibility
+        // Network calls have been removed from this mod
+        Debug.Log("ApeX Camera Mod v1.1.3 loaded successfully");
     }
 }
