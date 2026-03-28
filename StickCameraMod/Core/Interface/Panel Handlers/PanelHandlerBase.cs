885a69ed7e7470a8953e9755e79ca620907fb452
St1xk <stickgtreel@gmail.com>
2026-03-28 21:02:43 +0100

    Deleted StickCameraMod directory

--- StickCameraMod/Core/Interface/Panel Handlers/PanelHandlerBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace StickCameraMod.Core.Interface.Panel_Handlers;
-
-public class PanelHandlerBase : MonoBehaviour
-{
-    protected virtual void Start() => gameObject.SetActive(false);
-}
